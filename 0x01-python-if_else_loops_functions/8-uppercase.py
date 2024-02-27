6b80eecf37824c80708f21ab90337613f40e9532
ahmedssamy11555 <ahmedssamy11555@gmail.com>
2024-02-27 18:35:09 +0200

    uppercase+++

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -3,12 +3,12 @@ def uppercase(str):
     length = 0
     for _ in str:
 
-        length+=1
+        length += 1
 
-    result= ""
-    for charIndex in range(length ):
+    result = ""
+    for charIndex in range(length):
         if ord('a') <= ord(str[charIndex]) <= ord('z'):
-            result += chr(ord(str[charIndex]) - 32 )
+            result += chr(ord(str[charIndex]) - 32)
         else:
             result += str[charIndex]
     print('{}'.format(result))
