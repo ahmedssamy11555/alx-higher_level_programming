4c71ad74966f6426cdccdcab5959a112a7493e08
ahmedssamy11555 <ahmedssamy11555@gmail.com>
2024-02-29 15:02:23 +0200

    How to make a script dynamic++

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,27 +2,23 @@
 import sys
 
 
-def main():
-    def print_args(argu):
-        print("{} {}:".format(args_length, argu))
-        for i, arg in enumerate(args_list):
-            if i == 0:
-                continue
-            print("{}:".format(i), end=" ")
-            print("{}".format(arg))
+def print_args(argu, args_list, args_length):
+    print("{} {}:".format(args_length, argu))
+    for i, arg in enumerate(args_list):
+        print("{}:".format(i+1), end=" ")
+        print("{}".format(arg))
 
+
+def main():
     args_list = sys.argv
     args_length = len(args_list)
-    if args_length < 2:
-        args_length = 0
+    if args_length  == 0:
         print("{} arguments.".format(args_length))
     elif args_length > 0:
-        args_length -= 1
-
         if args_length == 1:
-            print_args("argumnent")
+            print_args("argumnent", args_list, args_length)
         else:
-            print_args("argumnents")
+            print_args("argumnents", args_list, args_length)
 
 
 if __name__ == "__main__":
