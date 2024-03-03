5b5bcd5b6978786ad1664c262b668949351bf8e1
ahmedssamy11555 <ahmedssamy11555@gmail.com>
2024-03-03 19:28:34 +0200

    Replace in a copy+

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -5,10 +5,3 @@ def new_in_list(my_list, idx, element):
         return list_copy
     list_copy[idx] = element
     return list_copy
-my_list = [1, 2, 3, 4, 5]
-idx = 3
-new_element = 9
-new_list = new_in_list(my_list, idx, new_element)
-
-print(new_list)
-print(my_list)
