e553168b8c83eb1c157c1485f4ce2fc90374b3ec
ahmedssamy11555 <ahmedssamy11555@gmail.com>
2024-03-04 23:34:47 +0200

    Search and replace

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    replaced_list = my_list[:]
+    for i, element in enumerate(replaced_list):
+        if element == search:
+            replaced_list[i] = replace
+
+    return replaced_list
