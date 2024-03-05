4e1efcb3fd95d2791fb7dcf4c8a962a45029cb61
ahmedssamy11555 <ahmedssamy11555@gmail.com>
2024-03-05 11:05:45 +0200

    Simple delete by key++

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-def print_sorted_dictionary(a_dictionary):
-    sorted_dictionary = dict(sorted(a_dictionary.items()))
-
-    for key, value in sorted_dictionary.items():
-        print("{}: {}".format(key, value))
+def simple_delete(a_dictionary, key=""):
+    if not key or key not in a_dictionary:
+        return a_dictionary
+    del a_dictionary[key]
+    return a_dictionary
