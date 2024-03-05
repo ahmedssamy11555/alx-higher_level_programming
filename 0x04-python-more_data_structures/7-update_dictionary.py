30751566a3e719bcd771ab38e876375c431e6574
ahmedssamy11555 <ahmedssamy11555@gmail.com>
2024-03-05 10:52:57 +0200

    Update dictionary+

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
-    updated_dict = a_dictionary.copy()
-    for old_key, old_value in updated_dict.items():
-        if old_key == key:
-            updated_dict.update({key: value})
-    return updated_dict
+    a_dictionary[key] = value
+    return a_dictionary
