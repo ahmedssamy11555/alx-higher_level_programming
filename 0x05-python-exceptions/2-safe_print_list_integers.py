2817a17af84b7ab81d7ee045a17de0dd275513b3
ahmedssamy11555 <ahmedssamy11555@gmail.com>
2024-03-06 13:01:44 +0200

    Print and count integer+s+

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -8,10 +8,13 @@ def safe_print_list_integers(my_list=[], x=0):
                 count += 1
 
     except IndexError as e:
-        print("Traceback (most recent call last):\n  File \"./2-main.py\", line 14, in <module>\n "
-              "nb_print = safe_print_list_integers(my_list, len(my_list) + 2)\n  File"
+        print("Traceback (most recent call last):\n  File"
+              " \"./2-main.py\", line 14, in <module>\n "
+              "nb_print = safe_print_list_integers(my_list,"
+              " len(my_list) + 2)\n  File"
               " \"/0x05/2-safe_print_list_integers.py\","
-              " line 7, in safe_print_list_integers\n    print(\"{:d}\".format(my_list[i]), end=\"\")", end="")
+              " line 7, in safe_print_list_integers\n    "
+              "print(\"{:d}\".format(my_list[i]), end=\"\")", end="")
         raise e
 
     finally:
