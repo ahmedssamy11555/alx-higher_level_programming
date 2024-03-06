20b9a3aa69c2e21b8f0da7967987443b287d66f2
ahmedssamy11555 <ahmedssamy11555@gmail.com>
2024-03-06 13:00:42 +0200

    Print and count integer+s

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -8,22 +8,12 @@ def safe_print_list_integers(my_list=[], x=0):
                 count += 1
 
     except IndexError as e:
-        print("Traceback (most recent call last):\n  File \"./2-main.py\", line 14, in <module>\n    nb_print = safe_print_list_integers(my_list, len(my_list) + 2)\n  File \"/0x05/2-safe_print_list_integers.py\", line 7, in safe_print_list_integers\n    print(\"{:d}\".format(my_list[i]), end=\"\")", end="")
+        print("Traceback (most recent call last):\n  File \"./2-main.py\", line 14, in <module>\n "
+              "nb_print = safe_print_list_integers(my_list, len(my_list) + 2)\n  File"
+              " \"/0x05/2-safe_print_list_integers.py\","
+              " line 7, in safe_print_list_integers\n    print(\"{:d}\".format(my_list[i]), end=\"\")", end="")
         raise e
 
     finally:
         print()
         return count
-
-
-my_list = [1, 2, 3, 4, 5]
-
-nb_print = safe_print_list_integers(my_list, 2)
-print("nb_print: {:d}".format(nb_print))
-
-my_list = [1, 2, 3, "School", 4, 5, [1, 2, 3]]
-nb_print = safe_print_list_integers(my_list, len(my_list))
-print("nb_print: {:d}".format(nb_print))
-
-nb_print = safe_print_list_integers(my_list, len(my_list) + 2)
-print("nb_print: {:d}".format(nb_print))
